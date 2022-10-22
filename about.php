dabbeb85daad447b475b48c45efc6028f345c97a
MainDjohny <lynx87@ukr.net>
2022-10-22 22:24:19 +0300

    MVC

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>Сontact form</title>
+</head>
+<body>
+    <?php require "blocks/header.php" ?>
+    <div class="container">
+    <h3>Сontact form</h3>
+    <form action="check.php" method="post">
+        <input type="email" name="email" placeholder="Input Email" class="form-control">
+        <textarea name="message" class="form-control" placeholder="Enter your message"></textarea>
+        <button type="submit" name="send" class="btn btn-success">Send</button>
+    </form>
+
+    </div>
+
+    <?php require "blocks/footer.php" ?>
+
+</body>
+</html>
---
+++ blocks/header.php
@@ -0,0 +1,12 @@
+<div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <span class="fs-4">Magento 2</span>
+      </a>
+
+      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Main</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Сontacts</a>
+      
+        <a class="btn btn-outline-primary" href="#">login</a>
+      </nav>
+    </div>
